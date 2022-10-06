5d67d5a603b8fc40d111e1d4fbb0609441bf1c65
Stephane PROUST <71001149+spr-github@users.noreply.github.com>
2022-10-06 17:44:39 +0200

    Update crap-file.ps1

--- crap-file.ps1
+++ crap-file.ps1
@@ -1,1 +1,2 @@
 $var = "https://testspr37.blob.core.windows.net/moisi2bd?sp=r&st=2022-10-06T15:35:02Z&se=2022-10-06T23:35:02Z&spr=https&sv=2021-06-08&sr=c&sig=ticQszTBUiKHdpCOHbP0UVKHVOsNbm9gouOpXhc%2FPqY%3D"
+$var2 = "https://testspr37.blob.core.windows.net/moisi2bd/test.txt?sp=r&st=2022-10-06T15:43:58Z&se=2022-10-06T23:43:58Z&spr=https&sv=2021-06-08&sr=b&sig=%2FOU3wLMdt3nkQC6gqkOg6WvE%2Bpp3yYfgl3NMdY82Zuo%3D"
