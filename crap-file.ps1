b7d186583945309297ac924c066b25ee7921b71a
Stephane PROUST <71001149+spr-github@users.noreply.github.com>
2022-10-06 17:36:24 +0200

    Create crap-file.ps1

---
+++ crap-file.ps1
@@ -0,0 +1,1 @@
+$var = "https://testspr37.blob.core.windows.net/moisi2bd?sp=r&st=2022-10-06T15:35:02Z&se=2022-10-06T23:35:02Z&spr=https&sv=2021-06-08&sr=c&sig=ticQszTBUiKHdpCOHbP0UVKHVOsNbm9gouOpXhc%2FPqY%3D"
